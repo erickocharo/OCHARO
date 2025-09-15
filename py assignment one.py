cc2beccb01b022f79a187cce6f4d117bdf55b1c8
erick ocharo <erickocharo134@gmail.com>
2025-09-15 18:02:34 +0300

    checkj

---
+++ py assignment one.py
@@ -0,0 +1,13 @@
+#name
+name="erick ocharo"
+#printinng name 
+print("my name is ",name)
+
+
+#age
+age=22
+print("youa age is :",age)
+
+#favourite toys 
+toys=["fitness tracker ","portable projector","adult LEGO"]
+print ("my favouritw toys are",toys)
